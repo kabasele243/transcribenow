ae8c67e36b4da258caf6b3859d23eda6d7d506f7
Franck <kabasele467@gmail.com>
2025-06-20 10:54:11 -0400

    allow upload s3

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,11 +1,19 @@
 import { S3Client } from '@aws-sdk/client-s3'
 
-export const s3Client = new S3Client({
-  region: process.env.AWS_REGION!,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  },
-})
+// Configure S3 client with access key authentication
+const createS3Client = () => {
+  const config = {
+    region: process.env.AWS_REGION!,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    }
+  }
 
+  return new S3Client(config)
+}
+
+export const s3Client = createS3Client()
+
+// Get bucket name from environment variable
 export const S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME! 
